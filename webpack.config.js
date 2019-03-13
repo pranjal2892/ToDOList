20d7f048c45e9341584f53d6c76a7dc58e8c3448
Divya Gupta <divgarg93@gmail.com>
2019-03-13 11:56:45 +0530

    index.html in public

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -18,11 +19,19 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test:/\.html$/,
+        use: [ 'html-loader']
       }
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
   ],
   resolve: {
     extensions: ['*', '.js', '.jsx']
